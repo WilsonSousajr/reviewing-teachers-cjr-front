5d18369cc3f8dbf27fa57fed561af5d7bc03a37c
Caselli <11felipe.caselli@gmail.com>
2024-05-14 10:30:13 -0300

    🎉 Started: Started unlogged feed

---
+++ src/pages/feed/unlogged/index.tsx
@@ -0,0 +1,9 @@
+export default function unllogedFeed(){
+    return(
+        <main>
+            <div id="unloggedHeader" className="w-screen h-16 bg-darkblue px-8 flex justify-between items-center">
+                
+            </div>
+        </main>
+    )
+}
