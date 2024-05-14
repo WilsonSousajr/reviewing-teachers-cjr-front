7b4ba18f3743e529190002eef9bb2a4188a054d5
Wilson Sousa <wstechnology.br@gmail.com>
2024-05-14 09:54:24 -0300

    ✨ Add: Create a UserPage component

---
+++ src/components/User/UserPage.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import UserProfileHeader from '@/components/User/UserProfileHeader';
+import UserPost from '@/components/User/UserPost';
+import { userAgent } from 'next/server';
+
+const UserPage: React.FC = () => {
+  const user = {
+    name: "Morty Gamer",
+    department: "Ciência da Computação / Dept. Ciência da Computação",
+    email: "Morty.gamer.23@cjr.org.br",
+    avatarUrl: "/public/profile-pic.png",
+  }
+
+
+    const posts = [
+        {
+            avatarUrl: "/public/profile-pic.png",
+            userName: "Morty Gamer",
+            date: "17/04/2024, às 21:42",
+            title: "João Frango - Surf",
+            content: "Contrary to popular belief, Lorem Ipsum is not simply random text...",
+            commentsCount: 2,
+        },
+        {
+            avatarUrl: "/public/profile-pic.png",
+            userName: "Morty Gamer",
+            date: "15/04/2024, às 21:42",
+            title: "Rick - Viagem Interdimensional",
+            content: "Contrary to popular belief, Lorem Ipsum is not simply random text...",
+            commentsCount: 5,
+        },
+    ];
+
+    return (
+        <div className="max-w-4xl mx-auto mt-8 p-4">
+            <UserProfileHeader 
+                name={user.name}
+                department= {user.department}
+                email={user.email}
+                avatarUrl={user.avatarUrl}	
+            />
+
+            <div className="mt-8">
+                <h2 className="text-xl font-bold mb-4">Publicações</h2>
+                {posts.map((post, index) => (
+                    <UserPost 
+                        key={index}
+                        avatarUrl={post.avatarUrl}
+                        userName={post.userName}
+                        date={post.date}
+                        title={post.title}
+                        content={post.content}
+                        commentsCount={post.commentsCount}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default UserPage;
