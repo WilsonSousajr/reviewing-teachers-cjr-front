e09fe83a8f57dcb6736f324942c1af50deb1da38
Felipe <11felipe.caselli@gmail.com>
2024-05-24 17:09:54 -0300

    🐛 BugFix: register font color

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -17,11 +17,11 @@ export default function Register () {
             </div>
             <div id="user-info" className="">
                 <form className="flex flex-col">
-                    <input className="placeholder:text-xl placeholder:pl-4 my-2 py-5 pr-96 rounded-xl" placeholder="Nome"></input>
-                    <input className="placeholder:text-xl placeholder:pl-4 my-2 py-5 pr-96 rounded-xl" placeholder="Email"></input>
-                    <input className="placeholder:text-xl placeholder:pl-4 my-2 py-5 pr-96 rounded-xl" placeholder="Senha"></input>
-                    <input className="placeholder:text-xl placeholder:pl-4 my-2 py-5 pr-96 rounded-xl" placeholder="Curso"></input>
-                    <input className="placeholder:text-xl placeholder:pl-4 my-2 py-5 pr-96 rounded-xl" placeholder="Departamento"></input>
+                    <input className="my-2 py-5 pr-96 rounded-xl text-black text-xl pl-4" placeholder="Nome"></input>
+                    <input className="my-2 py-5 pr-96 rounded-xl text-black text-xl pl-4" placeholder="Senha"></input>
+                    <input className="my-2 py-5 pr-96 rounded-xl text-black text-xl pl-4" placeholder="Email"></input>
+                    <input className="my-2 py-5 pr-96 rounded-xl text-black text-xl pl-4" placeholder="Curso"></input>
+                    <input className="my-2 py-5 pr-96 rounded-xl text-black text-xl pl-4" placeholder="Departamento"></input>
                 </form>
             </div> 
             <div className="flex justify-center gap-10">
