3efc61aefe57a03cd0980a3905214327d8407894
Leticia <leticiagbomfim22@gmail.com>
2024-05-29 16:43:12 -0300

    :wrench: Install formik

--- src/components/modals/comment.tsx
+++ src/components/modals/comment.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import {Formik} from 'formik';
 
 
 <div className="fixed z-50 top-0 left-0 w-full h-full bg-darkblue bg-opacity-50 flex justify-center items-center">
             <div className="bg-darkblue w-3/5 h-3/5 p-8 rounded-xl">
+        <Formik></Formik>
+
 
             </div>
 </div>
