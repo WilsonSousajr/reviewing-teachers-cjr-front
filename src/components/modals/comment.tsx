f48484567b41f53034f5a4ec0f48f6d415df5bd3
Leticia <leticiagbomfim22@gmail.com>
2024-05-29 16:56:24 -0300

    :sparkles:Add: Cancel and Send buttons

--- src/components/modals/comment.tsx
+++ src/components/modals/comment.tsx
@@ -4,7 +4,21 @@ import {Formik} from 'formik';
 
 <div className="fixed z-50 top-0 left-0 w-full h-full bg-darkblue bg-opacity-50 flex justify-center items-center">
             <div className="bg-darkblue w-3/5 h-3/5 p-8 rounded-xl">
-        <Formik></Formik>
+                <Formik 
+                    initialValues={{
+                     professorname: '',
+                     discipline: ''
+                        }
+                    }
+                        >
+                            
+                    <form>
+
+                        <button type="button" onClick={onclose} className="">Cancelar</button>
+                        <button type="submit" className="">Enviar</button>
+                    </form>
+
+                </Formik>
 
 
             </div>
