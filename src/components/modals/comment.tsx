9b8cb2dfbea0c2c9edf5316d4f65bfbfcd73b2d6
Leticia <leticiagbomfim22@gmail.com>
2024-05-29 16:25:53 -0300

    :sparkles:Add: comment box

--- src/components/modals/comment.tsx
+++ src/components/modals/comment.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
-<div className = "fixed z-50 top-0 left-0 w-full h-full bg-darkblue bg-opacity-50 flex justify-center items-center">
 
+<div className="fixed z-50 top-0 left-0 w-full h-full bg-darkblue bg-opacity-50 flex justify-center items-center">
+            <div className="bg-darkblue w-3/5 h-3/5 p-8 rounded-xl">
 
+            </div>
 </div>
