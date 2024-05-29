55f49b6ffcb767c1a7618e91762cb32848dc259d
Leticia <leticiagbomfim22@gmail.com>
2024-05-29 17:34:42 -0300

    :sparkles::lipstick:Add: const onclose and Edit buttons Cancelar and Avaliar

--- src/components/modals/comment.tsx
+++ src/components/modals/comment.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import {Formik} from 'formik';
 
+const onclose = () =>{
+    window.close();
+};
 
-<div className="fixed z-50 top-0 left-0 w-full h-full bg-darkblue bg-opacity-50 flex justify-center items-center">
-            <div className="bg-darkblue w-3/5 h-3/5 p-8 rounded-xl">
-                <Formik 
-                    initialValues={{
-                     professorname: '',
-                     discipline: ''
-                        }
-                    }
-                        >
-                            
-                    <form>
+     <div className="fixed z-50 top-0 left-0 w-full h-full bg-darkblue bg-opacity-50 flex justify-center items-center">
+        <div className="bg-darkblue w-3/5 h-3/5 p-8 rounded-xl">
 
-                        <button type="button" onClick={onclose} className="">Cancelar</button>
-                        <button type="submit" className="">Enviar</button>
-                    </form>
+                        <form>
 
-                </Formik>
-
-
+                             <button onClick={onclose} type="button" className="bg-darkblue text-white text-xl py-1 px-8 rounded-xl outline outline-white outline-2 shadow-black shadow-md">Cancelar</button>
+                             <button type="submit" className="bg-lightblue text-white text-xl py-1 px-8 rounded-xl outline outline-white outline-2 shadow-black shadow-md">Avaliar</button>
+                         </form>
             </div>
-</div>
+        </div>
