d8f1f2b6a0e5185965bfbdf4cc908c02324c689e
Felipe <11felipe.caselli@gmail.com>
2024-06-06 12:05:33 -0300

    ✨ Add: implement user fetch in reviews

--- src/components/Teacher/Reviews.tsx
+++ src/components/Teacher/Reviews.tsx
@@ -44,13 +44,15 @@ const Review: React.FC<Props> = ({ avatarUrl, userId, date, title, content }) =>
 
 return(
     <>
-        <div className="bg-white rounded-lg shadow-md p-6 mb-4">
+    {foundUser? (
+        user.map((thisUser: User, index) => (
+            <div key={index} className="bg-white rounded-lg shadow-md p-6 mb-4">
             <div className="flex items-center">
                 <div className="relative w-12 h-12">
                     <img className="rounded-full w-full h-full" src={avatarUrl} alt="Profile Picture" />
                 </div>
                 <div className="ml-4">
-                    <h3 className="font-bold">{userName}</h3>
+                        <h3 className="font-bold">{thisUser.name}</h3>
                     <p className="text-sm text-gray-600">{date} - {title}</p>
                 </div>
             </div>
@@ -66,6 +68,31 @@ return(
                 </div>
             </div>
         </div>
+        ))
+    ):(
+        <div key={1} className="bg-white rounded-lg shadow-md p-6 mb-4">
+                <div className="flex items-center">
+                    <div className="relative w-12 h-12">
+                        <img className="rounded-full w-full h-full" src={avatarUrl} alt="Finding User" />
+                    </div>
+                    <div className="ml-4">
+                        <h3 className="font-bold">Carregando Usuário</h3>
+                        <p className="text-sm text-gray-600">{date} - {title}</p>
+                    </div>
+                </div>
+                <p className="mt-4 text-gray-800">
+                    {content}
+                </p>
+                <div className="mt-4 flex justify-between">
+                    <div className="flex gap-4">
+                        <button className="flex items-center text-gray-600 hover:text-gray-800">
+                        </button>
+                    </div>
+                    <div className="flex gap-4">
+                    </div>
+                </div>
+            </div>
+    )}
 
         </>
     )
