f7243944c639b4d747225454447ba7a8d6ad2830
Felipe <11felipe.caselli@gmail.com>
2024-06-06 12:07:39 -0300

    ✨ Add: users info in review done

--- src/components/Teacher/Reviews.tsx
+++ src/components/Teacher/Reviews.tsx
@@ -47,27 +47,27 @@ return(
     {foundUser? (
         user.map((thisUser: User, index) => (
             <div key={index} className="bg-white rounded-lg shadow-md p-6 mb-4">
-            <div className="flex items-center">
-                <div className="relative w-12 h-12">
-                    <img className="rounded-full w-full h-full" src={avatarUrl} alt="Profile Picture" />
-                </div>
-                <div className="ml-4">
+                <div className="flex items-center">
+                    <div className="relative w-12 h-12">
+                        <img className="rounded-full w-full h-full" src={avatarUrl} alt="Profile Picture" />
+                    </div>
+                    <div className="ml-4">
                         <h3 className="font-bold">{thisUser.name}</h3>
-                    <p className="text-sm text-gray-600">{date} - {title}</p>
+                        <p className="text-sm text-gray-600">{date} - {title}</p>
+                    </div>
+                </div>
+                <p className="mt-4 text-gray-800">
+                    {content}
+                </p>
+                <div className="mt-4 flex justify-between">
+                    <div className="flex gap-4">
+                        <button className="flex items-center text-gray-600 hover:text-gray-800">
+                        </button>
+                    </div>
+                    <div className="flex gap-4">
+                    </div>
                 </div>
             </div>
-            <p className="mt-4 text-gray-800">
-                {content}
-            </p>
-            <div className="mt-4 flex justify-between">
-                <div className="flex gap-4">
-                    <button className="flex items-center text-gray-600 hover:text-gray-800">
-                    </button>
-                </div>
-                <div className="flex gap-4">
-                </div>
-            </div>
-        </div>
         ))
     ):(
         <div key={1} className="bg-white rounded-lg shadow-md p-6 mb-4">
@@ -94,7 +94,7 @@ return(
             </div>
     )}
 
-        </>
+    </>
     )
 }
 
