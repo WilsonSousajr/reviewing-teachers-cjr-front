c6e593256fd611f90208fd1c6dbe87ccfd1199ac
Felipe <11felipe.caselli@gmail.com>
2024-06-10 18:09:10 -0300

    Fix: fix components routes

--- src/pages/user/[id]/logged/index.tsx
+++ src/pages/user/[id]/logged/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
-import Header from "../../../components/Header/Header";
-import UserPage from "../../../components/User/UserPage";
+import Header from "../../../../components/Header/Header";
+import UserPage from "../../../../components/User/UserPage";
 
 const User: NextPage = () => {
   return (
