bffb322c92eb99b21348c638fa62ba833a89bae5
Felipe <11felipe.caselli@gmail.com>
2024-06-10 19:14:30 -0300

    Replicate: replicate unlloged user page to logged user page

--- src/components/User/UserPage.tsx
+++ src/components/User/UserPage.tsx
@@ -1,60 +1,55 @@
 import React from 'react';
 import UserProfileHeader from '@/components/User/UserProfileHeader';
 import UserPost from '@/components/User/UserPost';
-import { userAgent } from 'next/server';
 
-const UserPage: React.FC = () => {
-  const user = {
-    name: "Morty Gamer",
-    department: "Ciência da Computação / Dept. Ciência da Computação",
-    email: "Morty.gamer.23@cjr.org.br",
-    avatarUrl: "http://localhost:3000/images/profile-pic.png",
-    bannerUrl: "http://localhost:3000/images/banner.png"
+
+interface Review {
+    id: number;
+    userId: number;
+    teacherId: number;
+    disciplineId: number;
+    title: string;
+    content: string;
+    createdAt: string;
+    updatedAt: string;
   }
+interface Props{
+    userName: string;
+    userDepartament: string;
+    userEmail: string;
+    userPicture: string;
+    reviews: Review[]
+}
 
-
-    const posts = [
-        {
-            avatarUrl: "http://localhost:3000/images/profile-pic.png",
-            userName: "Morty Gamer",
-            date: "17/04/2024, às 21:42",
-            title: "João Frango - Surf",
-            content: "Contrary to popular belief, Lorem Ipsum is not simply random text...",
-            commentsCount: 2,
-        },
-        {
-            avatarUrl: "http://localhost:3000/images/profile-pic.png",
-            userName: "Morty Gamer",
-            date: "15/04/2024, às 21:42",
-            title: "Rick - Viagem Interdimensional",
-            content: "Contrary to popular belief, Lorem Ipsum is not simply random text...",
-            commentsCount: 5,
-        },
-    ];
-
+const UserPage: React.FC<Props> = ({userDepartament, userEmail, userName, userPicture, reviews}) => {
     return (
         <div className="max-w-4xl mx-auto mt-8 p-4">
             <UserProfileHeader 
-                name={user.name}
-                department= {user.department}
-                email={user.email}
-                avatarUrl={user.avatarUrl}	
-                bannerUrl={user.bannerUrl}
+                name={userName}
+                department= {userDepartament}
+                email={userEmail}
+                avatarUrl={userPicture}	
+                bannerUrl="http://localhost:3000/images/banner.png"
             />
 
             <div className="mt-8">
                 <h2 className="text-xl font-bold mb-4">Publicações</h2>
-                {posts.map((post, index) => (
-                    <UserPost 
+                {reviews.length > 0 ? (
+                    reviews.map((post: Review, index) => (
+                        <UserPost 
                         key={index}
-                        avatarUrl={post.avatarUrl}
-                        userName={post.userName}
-                        date={post.date}
+                        avatarUrl={userPicture}
+                        userName={userName}
+                        date={post.createdAt}
                         title={post.title}
                         content={post.content}
-                        commentsCount={post.commentsCount}
+                        commentsCount={0}
                     />
-                ))}
+                    ))
+                ) : (
+                    <p>Não há postagens ainda!</p>
+                )
+                }
             </div>
         </div>
     )
--- src/pages/user/[id]/logged/index.tsx
+++ src/pages/user/[id]/logged/index.tsx
@@ -1,14 +1,88 @@
 import type { NextPage } from "next";
 import Header from "../../../../components/Header/Header";
 import UserPage from "../../../../components/User/UserPage";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Review {
+  id: number;
+  userId: number;
+  teacherId: number;
+  disciplineId: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+}
+interface User{
+  id: number;
+  email: string;
+  password: string;
+  name: string;
+  picture: string;
+  departament: string;
+  course: string;
+  createdAt: string;
+  updatedAt: string;
+}
 
 const User: NextPage = () => {
+  const router = useRouter();
+    
+  const [reviews, setReviews] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [loadingUser, setLoadinguser] = useState(true);
+  let reviewRoute: string= "";
+  let userRoute: string= "";
+  
+  
+  useEffect(() => {
+      if(!router.isReady) return
+      const id = router.query;
+      reviewRoute = 'http://localhost:3333/reviews/user/'+id?.id
+      userRoute = 'http://localhost:3333/users/'+id?.id
+      async function getReviews() {
+          try {
+              const response = await axios.get(reviewRoute);
+              setReviews(response.data);
+              console.log("Fetched reviews: ", response.data);
+          } catch (error) {
+              console.error("Error fetching reviews:", error);
+          } finally {
+              setLoading(false);
+          }
+      }
+
+      async function getUser() {
+          try {
+              const response = await axios.get(userRoute)
+              setUsers(response.data)
+          }catch (error){
+              console.error("Error fetching teacher: ", error)
+          }finally {
+              setLoadinguser(false);
+          }
+      }
+      
+      getReviews();
+      getUser();
+  }, [router.isReady]);
   return (
     <>
       <Header />
-      <div>
-        <UserPage />
-      </div>
+      {!loadingUser? (
+        users.length > 0 ? (
+          users.map((user: User, index) => (
+            <div key={index}>
+              <UserPage userDepartament={user.departament} userEmail={user.email} userName={user.name} userPicture={user.picture} reviews={reviews} />
+            </div>
+          ))
+        ) : (<p className="flex justify-items-center">Usuario nao encontrado!</p>)
+      ) : (
+        <p>Carregando ...</p>
+      )}
     </>
     
   )
