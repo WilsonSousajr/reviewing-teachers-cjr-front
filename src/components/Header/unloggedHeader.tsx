df95f37c9422ed6c091d5d2a48320c80d8a08dac
Luisa <luoliveiraa09@gmail.com>
2024-06-12 16:58:28 -0300

    header

--- src/components/Header/unloggedHeader.tsx
+++ src/components/Header/unloggedHeader.tsx
@@ -1,24 +1,38 @@
-
-import MyButton from "../Button/MyButton"
-import logo from "/public/logo.png"
-
 import React from 'react'
 import Link from "next/link";
 
+interface Review {
+  id: number;
+  userId: number;
+  teacherId: number;
+  disciplineId: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+}
+interface User{
+  id: number;
+  email: string;
+  password: string;
+  name: string;
+  picture: string;
+  departament: string;
+  course: string;
+  createdAt: string;
+  updatedAt: string;
+}
 
 const UnloggedHeader = () => {
   return (
     <>
-      <div id="unloggedHeader" className="w-screen h-20 bg-darkblue px-8 flex justify-between items-center mb-8">
-        <Link href={"/"} passHref>
-          <img src="https://s3-alpha-sig.figma.com/img/7d4c/9277/04bd33df48766b783c0133aa94571ff9?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=divasakUB9H0YN6XEK8UhxepnNx17Hh4gZIAFU4ymNWzQLXFfdphajzFIKhuRtyFz9LU2j5lm2U6P9HU7MNksXVwWMBLbF8q81qaZE~z1nONsormljfivIBALEAq1wmgIgJPR3nOm7IYzPqP~KY6qt3fyQeTKw-QDYrb-~2fvePxlJ5JMUZLqQcU-dShKYZZcmJZ6NXx03KDcwxIjNrGPz9Or5BdN10Qht-z1j6dvQ84xZgVHmtzow7pEyNQVYAWo8H-Da6636kAlik5Q9NTdMPqNM0m42rlJo84bnl4n5UqQwYHNd4TU6kZUnckuGAxwzaH9sPPJDuyX0IQLQVmtQ__" alt="UnB"
-          width={75} height={75} />
-        </Link>
+    <div id="unloggedHeader" className="w-screen h-20 bg-darkblue px-8 flex justify-between items-center mb-8">
+        <img src="https://s3-alpha-sig.figma.com/img/7d4c/9277/04bd33df48766b783c0133aa94571ff9?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=divasakUB9H0YN6XEK8UhxepnNx17Hh4gZIAFU4ymNWzQLXFfdphajzFIKhuRtyFz9LU2j5lm2U6P9HU7MNksXVwWMBLbF8q81qaZE~z1nONsormljfivIBALEAq1wmgIgJPR3nOm7IYzPqP~KY6qt3fyQeTKw-QDYrb-~2fvePxlJ5JMUZLqQcU-dShKYZZcmJZ6NXx03KDcwxIjNrGPz9Or5BdN10Qht-z1j6dvQ84xZgVHmtzow7pEyNQVYAWo8H-Da6636kAlik5Q9NTdMPqNM0m42rlJo84bnl4n5UqQwYHNd4TU6kZUnckuGAxwzaH9sPPJDuyX0IQLQVmtQ__" alt="UnB"
+        width={75} height={75} />
         <div>
-
-            <Link href="/login" passHref>
-              <MyButton text="Login" className="bg-neutral-500 text-lg text-white px-8 py-2 rounded-lg outline outline-2 outline-white"></MyButton>
-            </Link>
+        <Link href="/" passHref>
+            <button className="bg-neutral-500 text-lg text-white px-8 py-2 rounded-lg outline outline-2 outline-white">Login</button>
+        </Link>
         </div>
     </div>   
     </>
