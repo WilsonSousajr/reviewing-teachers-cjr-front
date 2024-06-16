5719cb9015275d3feea70b0fb43e085fa0970943
Wilson Sousa <wstechnology.br@gmail.com>
2024-06-16 02:30:47 -0300

    ✨ Add: New index page logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import Header from "@/components/Header/Header";
 import UnloggedHeader from "@/components/Header/UnloggedHeader";
 import Feed from "@/components/Feed/feed";
 
-export default function unllogedFeed(){
-    return(
-        <main>
-            <UnloggedHeader />    
-            <Feed />    
-        </main>
-    )
-}
+const HomePage: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
+  return (
+    <main>
+      {isLoggedIn ? <Header /> : <UnloggedHeader />}
+      <Feed />
+    </main>
+  );
+};
+
+export default HomePage;
